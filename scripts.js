3474bf8f0c479798589268c0ad942ec0c9fa5b17
Waltari10 <vallai10@gmail.com>
2016-03-07 15:00:22 +0200

    testing some tings

--- scripts.js
+++ scripts.js
@@ -1,37 +1,36 @@
 
-var ctx, canvas;
+app = {
+    canvas: null,
+    ctx: null,
+    init: function () {
+        var imageLoader = document.getElementById('imageLoader');
+         imageLoader.addEventListener('change', app.handleImage, false);
 
-function init() {
-	var imageLoader = document.getElementById('imageLoader');
-		 imageLoader.addEventListener('change', handleImage, false);
+        canvas = document.getElementById('appCanvas');
+        ctx = canvas.getContext('2d');
+    },
 
-	canvas = document.getElementById('appCanvas');
-	ctx = canvas.getContext('2d');
-}
-/* important! for alignment, you should make things
- * relative to the canvas' current width/height.
- */
-function draw() {
-	ctx.canvas.width  = window.innerWidth;
-	ctx.canvas.height = window.innerHeight;
-	//...drawing code...
-	drawRectangle();
-}
+    draw: function () {
+        ctx.canvas.width  = window.innerWidth;
+        ctx.canvas.height = window.innerHeight;
+        //...drawing code...
+        app.drawRectangle();
+    },
 
-function drawRectangle() {
-	ctx.fillStyle = "#FF0000";
-	ctx.fillRect(0,0,150,75);
-}
+    drawRectangle: function() {
+        ctx.fillStyle = "#FF0000";
+        ctx.fillRect(0,0,150,75);
+    },
 
-function handleImage(e) {
-	var reader = new FileReader();
-	reader.onload = function(event) {
-		var img = new Image();
-		img.onload = function() {
-			ctx.drawImage(img, 0,0);
-		}
-		img.src= event.target.result;
-	}
-	reader.readAsDataURL(e.target.files[0])
-;
+    handleImage: function(e) {
+        var reader = new FileReader();
+        reader.onload = function(event) {
+            var img = new Image();
+            img.onload = function() {
+                    ctx.drawImage(img, 0,0);
+            }
+            img.src= event.target.result;
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    }
 }
